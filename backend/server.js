5e56557545c58ef47f759688f57ee4a2e6209bd9
Kjamwal1 <jamwalk137@gmail.com>
2025-08-12 14:06:36 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(
   cors({
     origin: 'https://career-hub-25.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
